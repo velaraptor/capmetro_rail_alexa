139878d1556cd76ab6d34dee738e502ef1a67c34
velaraptor <info@christophvel.com>
2019-03-11 02:47:18 -0500

    fixes

--- lambda/run_metro.py
+++ lambda/run_metro.py
@@ -5,6 +5,7 @@
 import logging
 
 from ask_sdk_core.skill_builder import SkillBuilder, CustomSkillBuilder
+from ask_sdk_core.api_client import DefaultApiClient
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
@@ -17,7 +18,7 @@ from ask_sdk_model.services.reminder_management import Trigger, TriggerType, Ale
     PushNotification, PushNotificationStatus, ReminderRequest
 from ask_sdk_model.services import ServiceException
 import pytz
-import datetime
+from datetime import datetime
 
 sb = CustomSkillBuilder(api_client=DefaultApiClient())  # required to use remiders
 
@@ -48,21 +49,6 @@ class LaunchRequestHandler(AbstractRequestHandler):
         return handler_input.response_builder.response
 
 
-class AboutIntentHandler(AbstractRequestHandler):
-    """Handler for about intent."""
-
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("AboutIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In AboutIntentHandler")
-
-        handler_input.response_builder.speak((commands.ABOUT))
-        return handler_input.response_builder.response
-
-
 class NextTrainIntentHandler(AbstractRequestHandler):
     """Handler for times intent."""
 
@@ -83,7 +69,7 @@ class NextTrainIntentHandler(AbstractRequestHandler):
                                                                                'arrival_time_local',
                                                                                'departure_time_local',
                                                                                'day_indicator'])
-
+        set_reminder = True
         if not check:
             speech = ("The next {line} coming to {departing_station} will leave at "
                       "{departure_time_local} {day_indicator} and arrive at the {arrival_station}"
@@ -93,6 +79,7 @@ class NextTrainIntentHandler(AbstractRequestHandler):
             speech = speech + time_to_get_there
         else:
             speech = 'Could not find times for that station.'
+            set_reminder = False
 
         check_2 = all(response[value] is None for value in second_train if value in ['arrival_time_epoch',
                                                                                      'departure_time_epoch',
@@ -104,12 +91,12 @@ class NextTrainIntentHandler(AbstractRequestHandler):
         else:
             second_train_speech = ''
         speech = speech + second_train_speech
-        speech = speech + '. Do you want to set a reminder?'
+        speech = speech + '. Do you want to set a reminder?' if set_reminder else speech
         logger.info(speech)
-
+        ask_question = 'Do you want to set a reminder?' if set_reminder else 'Anything else I can do?'
         session_attr['station'] = response['line']
         session_attr['walking_time'] = tz_dict['epoch']
-        handler_input.response_builder.speak(speech).ask('Do you want to set a reminder?')
+        handler_input.response_builder.speak(speech).ask(ask_question)
         return handler_input.response_builder.response
 
 
@@ -139,13 +126,15 @@ class YesMoreInfoIntentHandler(AbstractRequestHandler):
         tz = pytz.timezone('America/Chicago')
         nt = datetime.fromtimestamp(session_attr['walking_time']).astimezone(tz)
         notification_time = nt.strftime("%Y-%m-%dT%H:%M:%S")
+        logger.info("Notification Time {}".format(notification_time))
 
         trigger = Trigger(TriggerType.SCHEDULED_ABSOLUTE, notification_time, time_zone_id=TIME_ZONE_ID)
-        text = SpokenText(locale='en-US', ssml='<speak>This is your reminder</speak>', text='This is your reminder')
+        text = SpokenText(locale='en-US', ssml='<speak>This is your reminder to leave to get the next train</speak>',
+            text='This is your reminder to leave to get the next train')
         alert_info = AlertInfo(SpokenInfo([text]))
         push_notification = PushNotification(PushNotificationStatus.ENABLED)
         reminder_request = ReminderRequest(notification_time, trigger, alert_info, push_notification)
-
+        logger.info(reminder_request)
         try:
             reminder_responce = reminder_service.create_reminder(reminder_request)
         except ServiceException as e:
@@ -154,7 +143,7 @@ class YesMoreInfoIntentHandler(AbstractRequestHandler):
             raise e
 
         return rb.speak("reminder created") \
-            .set_card(SimpleCard("Notify Me", "reminder created")) \
+            .set_card(SimpleCard("Notify Me", "leave to get the next train")) \
             .set_should_end_session(True) \
             .response
 
@@ -250,7 +239,6 @@ sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(YesMoreInfoIntentHandler())
 sb.add_request_handler(NoMoreInfoIntentHandler())
 sb.add_request_handler(NextTrainIntentHandler())
-sb.add_request_handler(AboutIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(ExitIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
